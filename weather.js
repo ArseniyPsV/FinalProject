33dbab41f186c559540c842549d2521b51f724ab
ArseniyPsV <157610525+ArseniyPsV@users.noreply.github.com>
2025-05-14 18:32:46 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,6 +1,5 @@
 /**
  * Обновляет информацию о погоде в Минске.
- * @function updateWeather
  */
  function updateWeather() {
     const apiKey = 'c59824b56f5db34d02d956aa43a5c7a4';
@@ -20,11 +19,7 @@
                 const weatherInfo = document.getElementById('weather-info');
                 weatherInfo.innerHTML = `<i class="fas fa-thermometer-half"></i> Температура: ${temp} °C<br>Описание: ${description}`;
             })
-            .catch(error => {
-                console.error('Ошибка:', error);
-                const weatherInfo = document.getElementById('weather-info');
-                weatherInfo.innerHTML = `<i class="fas fa-thermometer-half"></i> Ошибка при получении данных о погоде`;
-            });
+            
     } catch (error) {
         console.error('Ошибка:', error);
         const weatherInfo = document.getElementById('weather-info');
